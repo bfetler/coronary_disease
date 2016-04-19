fe8af45d8a7083d6270e123cac6d7a6e9652c045
BFetler <bfetler.gmail.com>
2016-04-19 00:24:52 -0400

    fix histogram plot

--- coronary_predict.py
+++ coronary_predict.py
@@ -34,8 +34,9 @@ def read_data(name):
     # check for ? missing values - six of 'em
     # check for just ? or any non-numeric character
     for c in col_names:
-        df[c] = df[c].apply(lambda s: np.nan if s=='?' else s)
+        df[c] = df[c].apply(lambda s: np.nan if s=='?' else float(s))
     df = df.dropna()
+    # maybe columns w/ ? are str not int, need to convert
     df['Y'] = df['risk'].apply(lambda x: 1 if x>0 else 0)
     print("raw df shape %s head\n%s" % (df.shape, df[:3]))
 #    print("raw df stats\n%s" % df.describe())
@@ -61,6 +62,23 @@ def load_data(name, print_out=True):
         print("train_y stats\n%s\ntest_y stats\n%s" % (train_y.describe(), test_y.describe()))
     return train, test, train_y, test_y
 
+def scale_data(train_X, test_X, print_out=False):
+    '''Scale data for transform, read dataframes, return nparrays.'''
+    scaler = StandardScaler()
+    train_X = scaler.fit_transform(train_X)  # nparray
+    test_X  = scaler.transform(test_X)
+    if print_out:
+        print("scaler mean %s\nscaler std %s" % (scaler.mean_, scaler.scale_))
+        print("train means %s" % [train_X[:, j].mean() for j in range(train_X.shape[1]) ])
+        print("train std %s" % [train_X[:, j].std() for j in range(train_X.shape[1]) ])
+        print("test means %s" % [test_X[:, j].mean() for j in range(test_X.shape[1]) ])
+        print("test std %s" % [test_X[:, j].std() for j in range(test_X.shape[1]) ])
+        # train_X columns are scaled by definition, test_X off by 1-2% (not surprising)
+        print("train_X mean %.5f std %.5f" % (train_X.mean(), train_X.std()))
+        print("test_X mean %.5f std %.5f" % (test_X.mean(), test_X.std()))
+    # scaler.inverse_transform(train_X)
+    return train_X, test_X
+
 def test_incoming(test_X, train_X):
     '''Pretend we have streaming data coming in.  Run a few
        statistical tests on it, see if it's close to training data.'''
@@ -74,19 +92,13 @@ def plot_scatter_matrix(df, plotdir):
 def plot_hists(df, plotdir, ncols=3):
     plt.clf()
     vlist = list(df.columns)
-    vlist.pop()  # last itme has only one bin
-    print("vlist", vlist)
     nrows = len(vlist) // ncols
     if len(vlist) % ncols > 0:
         nrows += 1
     for i, var in enumerate(vlist):
         plt.subplot(nrows, ncols, i+1)
-        nbin = len(set(var))
-#        if nbin > 10:
-#            nbin = 10   # all < 10
-        print("var %s nbin %d" % (var, nbin))
-        plt.hist(df[var], bins=nbin)   # bins=30
-        plt.title(var, fontsize=10)
+        plt.hist(df[var].values, bins=15)
+        plt.title(var, fontsize=9)
         plt.tick_params(labelbottom='off', labelleft='off')
     plt.savefig(plotdir + 'hist_coronary.png')
 
@@ -115,28 +127,11 @@ def cross_validate(clf, train_X, train_y, print_out=False):
         print("  CV raw scores", scores)
     return score
 
-def scale_data(train_X, test_X, print_out=False):
-    '''Scale data for transform, read dataframes, return nparrays.'''
-    scaler = StandardScaler()
-    train_X = scaler.fit_transform(train_X)  # nparray
-    test_X  = scaler.transform(test_X)
-    if print_out:
-        print("scaler mean %s\nscaler std %s" % (scaler.mean_, scaler.scale_))
-        print("train means %s" % [train_X[:, j].mean() for j in range(train_X.shape[1]) ])
-        print("train std %s" % [train_X[:, j].std() for j in range(train_X.shape[1]) ])
-        print("test means %s" % [test_X[:, j].mean() for j in range(test_X.shape[1]) ])
-        print("test std %s" % [test_X[:, j].std() for j in range(test_X.shape[1]) ])
-        # train_X columns are scaled by definition, test_X off by 1-2% (not surprising)
-        print("train_X mean %.5f std %.5f" % (train_X.mean(), train_X.std()))
-        print("test_X mean %.5f std %.5f" % (test_X.mean(), test_X.std()))
-    # scaler.inverse_transform(train_X)
-    return train_X, test_X
-
 def main():
     train_X, test_X, train_y, test_y = load_data('cleveland', print_out=False)
-#    plotdir = make_plotdir()
+    plotdir = make_plotdir()
 #    plot_scatter_matrix(train_X, plotdir)  # takes a while, not that useful 
-#    plot_hists(train_X, plotdir)    # fails
+    plot_hists(train_X, plotdir)    # fails
     
     train_X, test_X = scale_data(train_X, test_X)
     
