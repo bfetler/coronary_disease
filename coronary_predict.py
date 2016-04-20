dca85e5da351d303c9dc91ad9fe729ef2be4da3c
BFetler <bfetler.gmail.com>
2016-04-20 19:17:55 -0400

    add test data histograms, t-tests

--- coronary_predict.py
+++ coronary_predict.py
@@ -8,6 +8,7 @@ from pandas.tools.plotting import scatter_matrix as pd_scatter_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.stats as sst
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split, cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
@@ -26,6 +27,7 @@ def make_plotdir():
     return plotdir
 
 def read_data(name):
+    "Read and clean data from file."
     f = "data/processed.%s.data" % (name)
     print("read %s" % f)
     col_names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', \
@@ -43,14 +45,13 @@ def read_data(name):
     return df
 
 def load_data(name, print_out=True):
+    "Read data and split into train, test data."
     df = read_data(name)
-#    train = df[:210]
-#    test  = df[210:]
     train, test = train_test_split(df, test_size=0.3)  # random_state=10
     yvars = ['risk', 'Y']
     train_y = train[yvars]
     test_y  = test[yvars]
-#    train_r = train['risk']    # use risk for five-way multi-class classification
+#    train_r = train['risk']    # for five-way multi-class classification
     train = train.drop(['risk', 'Y'], axis=1)
     test  = test.drop(['risk', 'Y'],  axis=1)
     if print_out:
@@ -63,7 +64,7 @@ def load_data(name, print_out=True):
     return train, test, train_y, test_y
 
 def scale_data(train_X, test_X, print_out=False):
-    '''Scale data for transform, read dataframes, return nparrays.'''
+    "Scale data for transform, read dataframes, return nparrays."
     scaler = StandardScaler()
     train_X = scaler.fit_transform(train_X)  # nparray
     test_X  = scaler.transform(test_X)
@@ -82,14 +83,22 @@ def scale_data(train_X, test_X, print_out=False):
 def test_incoming(test_X, train_X):
     '''Pretend we have streaming data coming in.  Run a few
        statistical tests on it, see if it's close to training data.'''
-    assert(0==0)   # to do
+    vlist = list(train_X.columns)
+    print("t-test compare train, test data\n    variable  pvalue   Pass")
+    print("    --------  ------   ----")
+    for var in vlist:
+        pval = sst.ttest_ind(test_X[var], train_X[var]).pvalue
+        print("    %-8s  %.4f   %s" % (var, pval, pval>0.05))
+#       assert(pval > 0.05)  # in production, assert each column passes
 
 def plot_scatter_matrix(df, plotdir):
+    "Plot scatter matrix."
     plt.clf()
     pd_scatter_matrix(df)
     plt.savefig(plotdir + 'scatter_matrix.png')
 
-def plot_hists(df, plotdir, ncols=3):
+def plot_hists(df, plotdir, label='x', ncols=3):
+    "Plot histograms of data columns in one plot."
     plt.clf()
     vlist = list(df.columns)
     nrows = len(vlist) // ncols
@@ -100,20 +109,21 @@ def plot_hists(df, plotdir, ncols=3):
         plt.hist(df[var].values, bins=15)
         plt.title(var, fontsize=9)
         plt.tick_params(labelbottom='off', labelleft='off')
-    plt.savefig(plotdir + 'hist_coronary.png')
+    plt.savefig(plotdir + 'hist_coronary_' + label + '.png')
 
 def confusion_report(test_y, new_y):
+    "Create classification report and confusion matrix."
     print("classification report\n%s" % classification_report(test_y['Y'], new_y))
     cm = confusion_matrix(test_y['Y'], new_y)
     print("confusion matrix\n%s" % cm)
 
 def fit_predict(clf, train_X, train_y, test_X, test_y, label='x'):
-#    print("fit shapes", train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+    "Fit train data, predict test data scores."
     clf.fit(train_X, train_y['Y'])
     fit_score = clf.score(train_X, train_y['Y'])
     pred_score = clf.score(test_X, test_y['Y'])
-#    new_y = clf.predict(test_X)
     print("%s: fit score %.5f, predict score %.5f" % (label, fit_score, pred_score))
+#    new_y = clf.predict(test_X)
 #    confusion_report(test_y, new_y)
     return pred_score
 
@@ -129,9 +139,12 @@ def cross_validate(clf, train_X, train_y, print_out=False):
 
 def main():
     train_X, test_X, train_y, test_y = load_data('cleveland', print_out=False)
+    test_incoming(test_X, train_X)
+    
     plotdir = make_plotdir()
 #    plot_scatter_matrix(train_X, plotdir)  # takes a while, not that useful 
-    plot_hists(train_X, plotdir)    # fails
+    plot_hists(train_X, plotdir, label='train')
+    plot_hists(test_X, plotdir, label='test')
     
     train_X, test_X = scale_data(train_X, test_X)
     
